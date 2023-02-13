25c31f1c8c7000d6d7e1c9172d54cf9965843f9c
mroman477 <125301931+mroman477@users.noreply.github.com>
2023-02-13 12:16:18 -0600

    Created using Popcode: https://popcode.org

---
+++ script.js
@@ -0,0 +1,41 @@
+
+
+let yellow = document.querySelector(".option-one");
+let screen = document.querySelector(".option-one-screen");
+let third = document.querySelector(".option-one1");
+let forth = document.querySelector(".option-two1");
+
+let blue = document.querySelector(".option-two");
+let screen2 = document.querySelector(".option-two-screen");
+let fifth = document.querySelector(".option-one2");
+let sixth = document.querySelector(".option-two2");
+
+let end1 = document.querySelector(".option-one1-end");
+let end2 = document.querySelector(".option-two-end");
+
+
+
+
+
+ //* When you're ready to make event handlers, uncomment the code below. Then fill in the blanks with the correct variables!
+
+yellow.onclick=function(){
+screen.style.display="block";
+};
+blue.onclick=function(){
+    end1.style.display="block";
+};
+third.onclick=function(){
+end2.style.display="block";
+};
+forth.onclick=function(){
+end2.style.display="block";
+};
+fifth.onclick=function(){
+screen2.style.display="block";
+};
+sixth.onclick=function(){
+screen2.style.display="block";
+};
+
+
